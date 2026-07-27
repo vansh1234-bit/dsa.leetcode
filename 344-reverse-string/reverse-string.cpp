e2fb1dd05798bf9a8d7c663311b2f3df94ebc0ee
vansh1234-bit <vanshpanwar1322@gmail.com>
2026-07-27 22:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (53.46%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int st = 0  ;
-        int end = s.size() - 1;
-        while(st <= end ){
-      swap(s[st] , s[end]) ;
-   st ++ ;
-   end -- ;
+void healper(vector<char> &s , int st , int end ){
+     if(st >= end ){
+            return ;
         }
+        swap(s[st] , s[end]) ;
+     healper(s ,  st + 1  , end - 1  ) ;
+
+}
+    void reverseString(vector<char>& s  ) {
+        int st = 0 ;
+        int end = s.size() -1 ;
+    return healper(s , st , end ) ;
     }
 };
