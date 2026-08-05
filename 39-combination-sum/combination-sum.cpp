90b580468d04e28dd10227b529f8f9892bc1f619
vansh1234-bit <vanshpanwar1322@gmail.com>
2026-08-05 22:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (94.07%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void findcombination(int idx , int target , vector<int> &arr  , vector<vector<int>> &ans , vector<int> &ds ){
+    if(idx == arr.size()){
+        if(target == 0 ){
+        ans.push_back(ds) ;
+    }
+    return ;
+    }
+    if(arr[idx] <= target ){
+        ds.push_back(arr[idx]) ;
+        findcombination(idx , target - arr[idx] , arr , ans , ds) ;
+        ds.pop_back();
+    }
+    findcombination(idx+1 , target , arr , ans , ds) ;
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans ;
+        vector<int> ds ;
+        findcombination(0 , target ,candidates ,  ans , ds ) ;
+        return ans ;
+    }
+};
