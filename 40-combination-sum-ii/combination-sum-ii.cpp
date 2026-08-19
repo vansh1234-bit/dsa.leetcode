1d410ced9440a4065857a97a92e41f9b901a7736
vansh1234-bit <vanshpanwar1322@gmail.com>
2026-08-19 15:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (65.57%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-void  comb(vector<int>& arr , int target , vector<int> &ds , int idx  , vector<vector<int>> &ans ){
-    if(target == 0 ){
-       ans.push_back(ds) ;
-       return ;
+void print(vector<int>& nums , int target , int idx , vector<int> &ds , vector<vector<int>> &ans ){
+if(target == 0 ){
+    ans.push_back(ds) ;
+    return ;
+}
+for(int i = idx ; i < nums.size() ; i++){
+    if(i > idx && nums[i] == nums[i-1]){
+        continue ;
     }
-    for(int i = idx ; i < arr.size() ; i++){
-        if(i > idx && arr[i-1] == arr[i]){
-            continue ;
-        }
-        if(target < arr[i]){
-            break ;
-        }
-        ds.push_back(arr[i]) ;
-        comb(arr , target - arr[i] , ds , i + 1  , ans  ) ;
-        ds.pop_back() ;
+    if(nums[i] > target){
+        break ;
     }
+    ds.push_back(nums[i]) ;
+    print(nums , target - nums[i] , i + 1 , ds , ans) ;
+    ds.pop_back() ;
+}
+
 }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        // int idx = 0 ;
         vector<int> ds ;
-        vector<vector<int>> ans ; 
-        sort(candidates.begin() , candidates.end()) ;
-        int idx = 0 ;
-         comb(candidates , target , ds , idx  , ans ) ;
-         return ans ;
+        vector<vector<int>> ans ;
+sort(candidates.begin() , candidates.end()) ;
+print(candidates , target , 0 , ds , ans ) ;
+return ans ;
+
     }
 };
