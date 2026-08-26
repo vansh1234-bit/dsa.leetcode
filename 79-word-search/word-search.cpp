7b23559bd73aecd75dd9092a813fb3e80717105d
vansh1234-bit <vanshpanwar1322@gmail.com>
2026-08-26 15:20:42 +0530

    Time: 959 ms (6.49%) | Memory: 11.8 MB (18.34%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-bool search(vector<vector<char>>& board, string &word , int i , int j , int st , vector<vector<int>> &visited){
-if(st == word.size()){
-    return true ;
-}
-if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size()){
+bool check(vector<vector<char>>& board, string word , int i , int j , int st , vector<vector<int >> &hash  ){
+    if(st == word.size()){
+        return true ;
+    }
+    if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size()){
+        return false ;
+    }
+    if(hash[i][j] == 1 || board[i][j] != word[st]) {
+        return false ;
+    }
+hash[i][j] = 1 ;
+    if(check(board , word , i+1 , j , st + 1 , hash  ) || check(board , word , i-1 , j , st + 1  , hash ) || check(board , word , i , j-1 , st + 1 ,hash ) || check(board , word , i , j + 1 , st + 1 , hash ) ){
+        return true ;
+    }
+    hash[i][j] = 0 ;
     return false ;
 }
-if(visited[i][j] || board[i][j] != word[st]){
-    return false ;
-}
-visited[i][j] = 1 ;
 
-bool found = search(board , word , i + 1 , j , st + 1 , visited ) || search(board , word , i - 1 , j , st + 1 , visited ) || search(board , word , i  , j + 1 , st + 1 , visited ) || search(board , word , i , j - 1, st + 1 , visited ) ;
-visited[i][j] = 0 ;
-return found ;
-}
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size() ;
+        int st = 0 ;
+        int i = 0 ;
+        int j = 0 ;
+        int m  = board.size() ;
         int n = board[0].size() ;
-        vector<vector<int>> visited(m , vector<int> (n , 0 )) ;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(search(board , word , i , j , 0 , visited)) {
-                    return true ;
-                }
+        vector<vector<int>> hash(m , vector<int>(n , 0 ) );
+        for(int i = 0 ; i < board.size() ; i++){
+            for(int j = 0 ; j < board[i].size() ; j++){
+               if (check(board ,word , i , j  , st, hash  ) ){
+                return true ;
+               } 
             }
-            // return false ;
         }
         return false ;
     }
