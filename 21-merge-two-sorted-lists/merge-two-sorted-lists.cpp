67bf5601f51bd94167c7ac2f19cc3837e6854691
vansh1234-bit <vanshpanwar1322@gmail.com>
2026-09-14 22:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (62.65%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -15,38 +15,43 @@ public:
             return list2 ;
         }
         if(list2 == NULL){
-            return list1 ;
+            return list1 ; 
         }
-         ListNode *ans ;
-         if(list1->val <= list2->val ){
-            ans = list1 ;
-            list1 = list1->next ;
-         }
-         else {
-         ans = list2 ;
-         list2 = list2->next ;
-         }
-         ListNode * temp = ans ;
-        while(list1 != NULL && list2 != NULL){
-if(list1->val <= list2->val){
-temp->next = list1 ;
-list1 = list1->next ;
-// temp = temp->next ; 
-}
-else {
-temp->next = list2 ;
-// list2->next = list1 ;
-list2 = list2->next ;
-// temp = temp->next ;
-}
-temp = temp->next ; 
+          ListNode *temp = NULL ;
+               ListNode *ans = temp ;
+            if(list1->val <= list2->val){
+               temp = list1 ;
+              
+            //    temp = temp->next ; 
+                list1 = list1->next ; 
+            }
+            else {
+                 temp = list2 ;
+                
+                list2 = list2->next ; 
+                // temp = temp->next ;
+            }
+            ans = temp ;
+        while(list1 != NULL && list2 != NULL ){
+            if(list1->val <= list2->val){
+                temp->next = list1 ;
+                list1 = list1->next ; 
+                // temp = temp->next ; 
+            }
+            else {
+                temp->next = list2 ;
+                list2 = list2->next ; 
+                // temp = temp->next ;
+            }
+            temp = temp->next ; 
         }
-        if(list1 == NULL){
-            temp->next = list2 ;
-        }
-        else if(list2 == NULL ){
+        if(list1 != NULL){
             temp->next = list1 ;
+            // temp = temp->next ;
+        } 
+        if(list2 != NULL ) {
+          temp->next = list2 ;
         }
-        return ans ;
+        return ans ; 
     }
 };
