7a3a5ec1a97095a9fcf4d32a35859ca937eb7c44
vansh1234-bit <vanshpanwar1322@gmail.com>
2026-09-17 22:54:43 +0530

    Time: 51 ms (82.68%) | Memory: 23.9 MB (85.43%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,42 +9,25 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL){
+            return NULL ; 
+        }
         ListNode * temp1 = headA ;
         ListNode * temp2 = headB ;
-        int count1 = 0 ;
-        int count2 = 0 ;
-        while(temp1!= NULL || temp2 != NULL){
-             if(temp1 != NULL){
-                count1 = count1 + 1 ;
-                temp1 = temp1->next ;
-             }
-             if(temp2 != NULL){
-                count2 = count2 + 1  ;
-                temp2 = temp2->next ;
-             }
-
+        while(temp1 != temp2){
+            temp1 = temp1->next ;
+            temp2= temp2->next ;
+            if(temp1 == temp2){
+                return temp1 ;
+            }
+            if(temp1 == NULL ){
+                temp1 = headB ;
+            }
+            if(temp2 == NULL){
+                temp2 = headA ;
+            }
+            
         }
-        int m = count1 - count2 ; 
-        ListNode * t1 = headA ;
-        ListNode * t2 = headB ;
-if(m >= 0 ){
-for(int i = 0 ; i < m ; i++){
-    t1= t1->next ;
-}
-}
-else {
-    for(int i = 0 ; i < -m ; i++){
-        t2= t2->next ; 
-    }
-}
-
-while(t1 != NULL){
-    if(t1 == t2){
-        return t1 ; 
-    }
-    t1=t1->next ;
-    t2=t2->next ; 
-}
-return NULL ; 
+          return temp1  ; 
     }
 };
