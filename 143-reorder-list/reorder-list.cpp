88e5de71e87e8110e0978ebc498302a771d3f6a0
vansh1234-bit <vanshpanwar1322@gmail.com>
2026-09-20 23:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (95.96%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -11,35 +11,40 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        ListNode * fast = head ;
-        ListNode * slow = head ;
-        while(fast->next != NULL && fast->next->next != NULL){
-            slow = slow->next ;
-            fast= fast->next->next ;
+        if(head == NULL || head->next == NULL){
+            return ; 
         }
-        if(fast->next != NULL){
-            fast = fast->next ; 
-        }
+     ListNode * slow = head ;
+     ListNode * t = head ; 
+     ListNode * fast = head ;
+     while(fast->next != NULL &&  fast->next->next != NULL){
+     fast = fast->next->next ;
+     slow = slow->next ;
 
-
-        ListNode * prev = NULL ;
-        ListNode* curr = slow->next ;
-        slow->next = NULL ; 
-        while(curr != NULL){
-            ListNode * nextnode = curr->next ;
-            curr->next = prev ;
-            prev = curr ;
-            curr = nextnode ; 
-        }
-        ListNode * first = head ;
-        ListNode * second = prev ;
-        while(second!= NULL ){
-            ListNode * firstNext = first->next ;
-            ListNode * secondNext = second->next ;
-            first->next = second;
-            second->next = firstNext ;
-            first = firstNext ;
-            second = secondNext ; 
-        }
+     }    
+     if(fast->next != NULL){
+        fast = fast->next ; 
+     }
+     ListNode * remove = slow->next ;
+     ListNode * temp = slow->next  ;
+     ListNode * prev = slow ;
+     while(temp != NULL ){
+    ListNode * next = temp->next ;
+    temp->next = prev ;
+    prev = temp ;
+    temp = next ; 
+     }
+     remove->next = NULL ;
+slow->next = NULL ;
+fast = prev ;
+while(fast != NULL ){
+ ListNode * tnext = t->next ;
+ ListNode * fnext = fast->next ;
+ t->next = fast ;
+t = tnext ;
+ fast->next = t;
+  
+ fast = fnext ; 
+} 
     }
 };
